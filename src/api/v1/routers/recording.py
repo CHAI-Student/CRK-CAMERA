d99cb0aece8519aa356facbe48ef8a9a4e55e4a7
손우진 <datmoyan@hanyang.ac.kr>
2026-02-02 15:12:59 +0900

    feat: simplify trigger_save_services handling in start and stop recording endpoints

--- src/api/v1/routers/recording.py
+++ src/api/v1/routers/recording.py
@@ -18,12 +18,11 @@ async def start_recording(request: Request, body: RecordingStartRequest):
     for key, service in save_services.items():
         await service.start(f"{body.save_path}/archival/cam_{key}")
 
-    trigger_save_services: dict[int, tuple[TriggerSaveService, TriggerSaveService]] = (
+    trigger_save_services: dict[int, TriggerSaveService] = (
         request.app.state.trigger_save_services
     )
-    for key, (top_service, side_service) in trigger_save_services.items():
-        await top_service.start(f"{body.save_path}/inference/zone_{key}")
-        await side_service.start(f"{body.save_path}/inference/zone_{key}")
+    for key, service in trigger_save_services.items():
+        await service.start(f"{body.save_path}/inference/zone_{key}")
     
     loadcell_service: LoadcellService = request.app.state.loadcell_service
     await loadcell_service.start()
@@ -33,12 +32,11 @@ async def start_recording(request: Request, body: RecordingStartRequest):
 
 @router.post("/stop")
 async def stop_recording(request: Request):
-    trigger_save_services: dict[int, tuple[TriggerSaveService, TriggerSaveService]] = (
+    trigger_save_services: dict[int, TriggerSaveService] = (
         request.app.state.trigger_save_services
     )
-    for key, (top_service, side_service) in trigger_save_services.items():
-        await top_service.stop()
-        await side_service.stop()
+    for key, service in trigger_save_services.items():
+        await service.stop()
 
     loadcell_service: LoadcellService = request.app.state.loadcell_service
     await loadcell_service.stop()
@@ -48,24 +46,3 @@ async def stop_recording(request: Request):
         await service.stop()
 
     return {"status": "recording stopped"}
-
-
-class RecordingTriggerRequest(BaseModel):
-    loadcell_idx: int = Field(..., ge=0, lt=10)
-    duration: float = Field(..., gt=0)
-
-
-@router.post("/trigger")
-async def trigger_recording(request: Request, body: RecordingTriggerRequest):
-    trigger_save_services: dict[int, tuple[TriggerSaveService, TriggerSaveService]] = (
-        request.app.state.trigger_save_services
-    )
-
-    top_service, side_service = trigger_save_services[body.loadcell_idx // 2 + 1]
-
-    top_service_event = await top_service.trigger(body.duration)
-    side_service_event = await side_service.trigger(body.duration)
-
-    # create a task to wait for the event to complete
-
-    return {"status": "recording triggered"}
